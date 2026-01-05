fb47b44cdc6b2889b5932b34f4bfc2270543ff58
Patrick Sharp <psharp@greenspice.com>
2026-01-04 21:38:30 -0800

    add sample files for filter test

---
+++ lua/waypoint/test/tests/filter/before.lua
@@ -0,0 +1,12 @@
+local weird_table = {       "one",    "two","three", 4       ,5,6,   "seven",}
+local config = {
+	very_long_property_name = "value", another_property = { 1, 2, 3 }, third_property = true,
+}
+local my_data = "single quotes"
+local array = { 
+"Lorem",
+"ipsum",
+"dolor",
+"sit",
+"amet"
+}
